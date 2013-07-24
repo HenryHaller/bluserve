e9534209901d80737404916238e4d6a6100346f3
= <=>
2013-07-23 20:28:07 -0400

    adding in kivy directory

---
+++ kivy/main.py
@@ -0,0 +1,32 @@
+import kivy
+kivy.require('1.7.1') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.gridlayout import GridLayout
+
+import net
+
+class Layout(GridLayout):
+    def regenerate_widgets(self):
+	db = net.get_database_from_server()
+	for address in db:
+		if db[address] == "authorize": btn = ToggleButton(text=address, group="device_addresses", state="down")
+		else: btn = ToggleButton(text=address, group="device_addresses")
+		btn.bind(on_press=lambda x: net.set_state(address))
+		self.add_widget(btn)
+    def __init__(self, **kwargs):
+	super(Layout, self).__init__(**kwargs)
+	self.cols=1
+	self.regenerate_widgets()
+	
+
+class MyApp(App):
+
+
+    def build(self):
+        return Layout()
+
+if __name__ == '__main__':
+    MyApp().run()
---
+++ kivy/net.py
@@ -0,0 +1,14 @@
+import json, urllib
+import httplib2
+HOST="192.168.2.25"
+PORT=8080
+
+def get_database_from_server():
+	url = "http://%s:%d/get_json" % (HOST, PORT)
+	return json.loads(urllib.urlopen(url).read())
+
+
+def set_state(device_address):
+	data = {"device_address": device_address}
+	encoded_data = urllib.urlencode(data)
+	urllib.urlopen("http://%s:%d/set_state" % (HOST, PORT), encoded_data)
