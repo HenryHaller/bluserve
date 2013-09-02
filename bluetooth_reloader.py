9c2f1e633ca88c78eb48d1f68dfc0ff4aeb6762e
Henry Haller <henrybhaller@gmail.com>
2013-09-02 03:59:56 +0000

    fix error of it not knowing when it had already restarted bluetoothd for a given error message

--- bluetooth_reloader.py
+++ bluetooth_reloader.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import subprocess, time
 while 1:
-	check_line = subprocess.check_output("tail -n1 /var/log/messages", shell=True)
-	if "[pulseaudio] module-loopback.c: Sample rates too different, not adjusting (44100 vs." in check_line:
+	check_blob = subprocess.check_output("tail -n3 /var/log/messages", shell=True)
+	if "[pulseaudio] module-loopback.c: Sample rates too different, not adjusting (44100 vs." in check_blob:
 		print "ERROR DETECTED, RESTARTING BLUETOOTH!!!"
 		subprocess.check_output("service bluetooth restart", shell=True)
 	time.sleep(0.5)
